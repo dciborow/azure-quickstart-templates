bb154f86b2d6b5bd3d614e45dd7227438f90d7cc
Daniel Ciborowski <dciborow@microsoft.com>
2022-07-26 01:01:17 -0400

    Update container_deploy.sh

--- quickstarts/microsoft.kubernetes/aks-helm/scripts/container_deploy.sh
+++ quickstarts/microsoft.kubernetes/aks-helm/scripts/container_deploy.sh
@@ -1,6 +1,20 @@
-AGREEMENT_URL="https://management.azure.com/subscriptions/edf507a2-6235-46c5-b560-fd463ba2e771/providers/Microsoft.Marketplace/offerTypes/microsoft-container/publishers/bitnami/offers/airflow-container/plans/image/agreements/current?api-version=2018-03-01-beta"
-CONFIG_URL="https://management.azure.com/subscriptions/edf507a2-6235-46c5-b560-fd463ba2e771/providers/Microsoft.Marketplace/offerTypes/microsoft-container/publishers/bitnami/offers/airflow-container/plans/image/configs/config8e95c8db-f45f-42e6-8d9b-8bfe67378d7b"
-IMPORT_URL="https://management.azure.com/subscriptions/edf507a2-6235-46c5-b560-fd463ba2e771/providers/Microsoft.Marketplace/offerTypes/microsoft-container/publishers/bitnami/offers/airflow-container/plans/image/configs/config8e95c8db-f45f-42e6-8d9b-8bfe67378d7b/importImage"
+SUBSCRIPTION_ID='edf507a2-6235-46c5-b560-fd463ba2e771'
+PUBLISHER='microsoftcorporation1590077852919'
+OFFER='horde-storage-container-preview'
+PLAN='storage-container-test'
+CONFIG_GUID='1dedfbed-4caa-42e8-bc0c-4e7d77707117'
+
+AGREEMENT_URL="https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/providers/Microsoft.Marketplace/offerTypes/microsoft-container/publishers/$PUBLISHER/offers/$OFFER/plans/$PLAN/agreements/current"
+CONFIG_URL="https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/providers/Microsoft.Marketplace/offerTypes/microsoft-container/publishers/$PUBLISHER/offers/$OFFER/plans/$PLAN/configs/config$CONFIG_GUID"
+IMPORT_URL="https://management.azure.com/subscriptions/$SUBSCRIPTION_ID/providers/Microsoft.Marketplace/offerTypes/microsoft-container/publishers/$PUBLISHER/offers/$OFFER/plans/$PLAN/configs/config$CONFIG_GUID/importImage"
+
+az rest --uri AGREEMENT_URL --method get \
+  --output-file agreement.json
+az rest --uri AGREEMENT_URL --method put \
+  --body @agreement.json
 
 az rest --uri $CONFIG_URL --method put \
   --body "{\"targetAcr\":\"$ACR_TARGET\",\"autoUpdate\":$ACR_AUTO_UPDATE,\"resourceGroup\":\"$ACR_RG\",\"tagOrDigest\":\"$ACR_TAG\"}"
+
+az rest --uri IMPORT_URL --method put \
+  --body "{\"registryResourceId\":\"/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$ACR_RG/providers/Microsoft.ContainerRegistry/registries/$ACR_TARGET\"}"
